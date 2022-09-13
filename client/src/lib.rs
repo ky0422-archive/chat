46ba62443f5df16eb8ba5fbbea897760df0661b1
tsukiroku <tsukiroku@yahoo.com>
2022-09-13 22:29:35 +0900

    Update Client

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,5 +1,5 @@
 use cursive::{views::*, *};
-use std::{io::*, net::*};
+use std::{io::*, net::*, sync::*};
 
 #[derive(Debug)]
 pub enum DialogType {
@@ -29,20 +29,26 @@ pub fn submit(writer: &mut BufWriter<TcpStream>, cursive: &mut Cursive) -> Resul
         return Ok(());
     }
 
-    if let None = cursive.call_on_name("chat", |view: &mut TextView| {
-        if view.get_content().source().trim() == "Enter your name" {
-            view.set_content(String::new());
-        }
-    }) {
-        return Ok(());
-    }
-
     writer.write(format!("{}\n", content.trim()).as_bytes())?;
     writer.flush()?;
 
-    if let None = cursive.call_on_name("scroll", |view: &mut ScrollView<NamedView<TextView>>| view.scroll_to_bottom()) {
-        return Ok(());
-    }
-
     Ok(())
+}
+
+pub fn submit_name(writer: Arc<Mutex<BufWriter<TcpStream>>>, cb_sink: CbSink, cursive: &mut Cursive) {
+    _ = cursive.call_on_name("client_name", |view: &mut EditView| match writer.lock() {
+        Ok(ref mut writer) => {
+            if let Err(e) = (|| -> Result<()> {
+                writer.write(format!("{}\n", view.get_content().trim()).as_bytes())?;
+                writer.flush()?;
+
+                Ok(())
+            })() {
+                _ = cb_sink.send(Box::new(move |s| dialog(s, format!("Error: Failed to send message:\n{e}"), DialogType::Error)));
+            }
+        }
+        _ => _ = cb_sink.send(Box::new(move |s| dialog(s, format!("Error: Failed to lock writer"), DialogType::Error))),
+    });
+
+    cursive.pop_layer();
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -31,27 +31,35 @@ fn main() {
         }
     });
     let writer = Arc::new(Mutex::new(BufWriter::new(stream)));
+    let writer_clone = writer.clone();
 
-    let layout = LinearLayout::vertical()
-        .child(Panel::new(scroll_view()))
-        .child(OnEventView::new(EditView::new().with_name("content")).on_event(Key::Enter, move |s| {
-            if let Err(e) = submit(
-                match &mut (writer.lock()) {
-                    Ok(writer) => writer,
-                    Err(e) => {
-                        dialog(s, format!("Error: Failed to lock writer:\n{e}"), DialogType::Error);
-                        return;
-                    }
-                },
-                s,
-            ) {
-                dialog(s, format!("Error: Failed to send message:\n{e}"), DialogType::Info);
-            }
-        }));
+    let layout = LinearLayout::vertical().child(Panel::new(scroll_view())).child(
+        OnEventView::new(EditView::new().with_name("content"))
+            .on_event(Key::Enter, move |s| {
+                if let Err(e) = submit(
+                    match &mut (writer.lock()) {
+                        Ok(writer) => writer,
+                        Err(e) => {
+                            dialog(s, format!("Error: Failed to lock writer:\n{e}"), DialogType::Error);
+                            return;
+                        }
+                    },
+                    s,
+                ) {
+                    dialog(s, format!("Error: Failed to send message:\n{e}"), DialogType::Info);
+                }
+            })
+            .on_event(EventTrigger::any(), |s| {
+                if let None = s.call_on_name("scroll", |view: &mut ScrollView<NamedView<TextView>>| view.scroll_to_bottom()) {
+                    return;
+                }
+            }),
+    );
 
     siv.add_fullscreen_layer(Dialog::around(layout).h_align(HAlign::Center).full_screen());
 
     let cb_sink = siv.cb_sink().clone();
+    let cb_sink_clone = cb_sink.clone();
 
     thread::spawn(move || loop {
         let mut line = String::new();
@@ -68,6 +76,14 @@ fn main() {
             }));
         }
     });
+
+    siv.add_layer(Dialog::around(
+        LinearLayout::vertical().child(Panel::new(TextView::new("Enter your name").with_name("chat"))).child(
+            OnEventView::new(EditView::new().with_name("client_name")).on_event(Key::Enter, move |s| {
+                submit_name(writer_clone.clone(), cb_sink_clone.clone(), s);
+            }),
+        ),
+    ));
 
     siv.run();
 }
